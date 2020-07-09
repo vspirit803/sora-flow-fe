a68c206f12884e3e21259837df60ee588126ab3f
vspirit803 <52484564+vspirit803@users.noreply.github.com>
2020-07-09 15:33:01 +0800

    fix(menu): fix the bug that active style doesn't take affect on top level menu

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -1,74 +1,74 @@
 <template>
   <v-list expand>
     <!--一级菜单-->
-    <template v-for="each of menus">
-      <v-list-item
-        v-if="each.type === 'item'"
-        :key="each.id"
-        link
-        :to="each.url"
-        @click="onSelect(each)"
-      >
-        <v-list-item-icon v-if="each.icon">
-          <v-icon>{{ each.icon }}</v-icon>
-        </v-list-item-icon>
-        <v-list-item-title>{{ each.name }}</v-list-item-title>
-      </v-list-item>
-      <v-list-group
-        v-else
-        :key="each.id"
-      >
-        <template v-slot:activator>
+    <v-list-item-group
+      color="primary"
+    >
+      <template v-for="each of menus">
+        <v-list-item
+          v-if="each.type === 'item'"
+          :key="each.id"
+          link
+          :to="each.url"
+        >
           <v-list-item-icon v-if="each.icon">
             <v-icon>{{ each.icon }}</v-icon>
           </v-list-item-icon>
           <v-list-item-title>{{ each.name }}</v-list-item-title>
-        </template>
-
-        <!--二级菜单-->
-        <template v-for="eachSub of each.children">
-          <v-list-item
-            v-if="eachSub.type === 'item'"
-            :key="eachSub.id"
-            link
-            :to="eachSub.url"
-            @click="onSelect(eachSub)"
-          >
-            <v-list-item-icon v-if="eachSub.icon">
-              <v-icon>{{ eachSub.icon }}</v-icon>
+        </v-list-item>
+        <v-list-group
+          v-else
+          :key="each.id"
+        >
+          <template v-slot:activator>
+            <v-list-item-icon v-if="each.icon">
+              <v-icon>{{ each.icon }}</v-icon>
             </v-list-item-icon>
-            <v-list-item-title>{{ eachSub.name }}</v-list-item-title>
-          </v-list-item>
-          <v-list-group
-            v-else
-            :key="eachSub.id"
-            sub-group
-          >
-            <template v-slot:activator>
+            <v-list-item-title>{{ each.name }}</v-list-item-title>
+          </template>
+
+          <!--二级菜单-->
+          <template v-for="eachSub of each.children">
+            <v-list-item
+              v-if="eachSub.type === 'item'"
+              :key="eachSub.id"
+              link
+              :to="eachSub.url"
+            >
               <v-list-item-icon v-if="eachSub.icon">
                 <v-icon>{{ eachSub.icon }}</v-icon>
               </v-list-item-icon>
               <v-list-item-title>{{ eachSub.name }}</v-list-item-title>
-            </template>
-
-            <!--三级菜单-->
-            <v-list-item
-              v-for="eachSubSub of eachSub.children"
-              :key="eachSubSub.id"
-              :input-value="eachSubSub.name === currMenu"
-              link
-              :to="eachSubSub.url"
-              @click="onSelect(eachSubSub)"
-            >
-              <v-list-item-icon v-if="eachSubSub.icon">
-                <v-icon>{{ eachSubSub.icon }}</v-icon>
-              </v-list-item-icon>
-              <v-list-item-title>{{ eachSubSub.name }}</v-list-item-title>
             </v-list-item>
-          </v-list-group>
-        </template>
-      </v-list-group>
-    </template>
+            <v-list-group
+              v-else
+              :key="eachSub.id"
+              sub-group
+            >
+              <template v-slot:activator>
+                <v-list-item-icon v-if="eachSub.icon">
+                  <v-icon>{{ eachSub.icon }}</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>{{ eachSub.name }}</v-list-item-title>
+              </template>
+
+              <!--三级菜单-->
+              <v-list-item
+                v-for="eachSubSub of eachSub.children"
+                :key="eachSubSub.id"
+                link
+                :to="eachSubSub.url"
+              >
+                <v-list-item-icon v-if="eachSubSub.icon">
+                  <v-icon>{{ eachSubSub.icon }}</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>{{ eachSubSub.name }}</v-list-item-title>
+              </v-list-item>
+            </v-list-group>
+          </template>
+        </v-list-group>
+      </template>
+    </v-list-item-group>
   </v-list>
 </template>
 
@@ -82,12 +82,7 @@ export default defineComponent({
   setup() {
     const store = useStore();
     const menus = computed(() => store.state.menus);
-    const currMenu = ref('');
-
-    function onSelect(item: { id: string; name: string }) {
-      currMenu.value = item.name;
-    }
-    return { menus, currMenu, onSelect };
+    return { menus };
   },
 });
 </script>
