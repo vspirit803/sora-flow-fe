e5ba00b31e1caf29babbf8202f69b59014cc3481
vspirit803 <52484564+vspirit803@users.noreply.github.com>
2020-08-27 16:13:39 +0800

    feat: add ICP record

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="home">
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <v-footer app>
+      <v-spacer />
+      <div>蜀ICP备20016613号-1</div>
+    </v-footer>
   </div>
 </template>
 
