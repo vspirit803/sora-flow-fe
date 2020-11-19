ce1890e70c796567c1318dcfdd22b5935bbd1c8d
vspirit803 <52484564+vspirit803@users.noreply.github.com>
2020-11-19 16:23:49 +0800

    feat: enable simple chinese language package

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -2,7 +2,12 @@ import '@mdi/font/css/materialdesignicons.css'; // Ensure you are using css-load
 
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
+import zhHans from 'vuetify/src/locale/zh-Hans';
 
 Vue.use(Vuetify);
-
-export default new Vuetify({});
+export default new Vuetify({
+  lang: {
+    locales: { zhHans },
+    current: 'zhHans',
+  },
+});
