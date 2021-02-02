5cd60493bb346ce0c98e07c5b42879390fdd508b
vspirit803 <lx546488173@gmail.com>
2021-02-02 17:39:48 +0800

    feat: update the introduction

--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -21,22 +21,26 @@
           justify="center"
           class="mb-3"
         >
-          <span
+          <a
             v-for="(tech, i) in technologyStack.frontEnd"
             :key="i"
             class="subheading mx-3"
+            :href="tech.link"
+            target="blank"
           >
-            {{ tech }}
-          </span>
+            {{ tech.name }}
+          </a>
         </v-row>
         <v-row justify="center">
-          <span
+          <a
             v-for="(tech, i) in technologyStack.backEnd"
             :key="i"
             class="subheading mx-3"
+            :href="tech.link"
+            target="blank"
           >
-            {{ tech }}
-          </span>
+            {{ tech.name }}
+          </a>
         </v-row>
       </v-col>
 
@@ -70,10 +74,20 @@ export default Vue.extend({
   name: 'Introduction',
   data: () => ({
     technologyStack: {
-      frontEnd: ['Vuejs', 'Vue Composition Api', 'Vue Router', 'Vuex', 'Vuetify'],
-      backEnd: ['Node.js', 'NestJS', 'MongoDB'],
+      frontEnd: [
+        { name: 'Vuejs', link: 'https://github.com/vuejs/vue' },
+        { name: 'Vue Composition Api', link: 'https://github.com/vuejs/composition-api' },
+        { name: 'Vue Router', link: 'https://github.com/vuejs/vue-router' },
+        { name: 'Vuex', link: 'https://github.com/vuejs/vuex' },
+        { name: 'vue-i18n', link: 'https://github.com/kazupon/vue-i18n' },
+      ],
+      backEnd: [
+        { name: 'Node.js', link: 'https://nodejs.org/' },
+        { name: 'NestJS', link: 'https://github.com/nestjs/nest' },
+        { name: 'MongoDB', link: 'https://www.mongodb.com/' },
+      ],
     },
-    next: ['表单表格字段展示', '各处表格增加筛选', '表单选项增加动态获取', '增加流程相关功能'],
+    next: ['本地化', '各处表格增加筛选', '表单选项增加动态获取', '增加流程相关功能'],
   }),
 });
 </script>
